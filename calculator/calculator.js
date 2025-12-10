9437d4d2466e3b9d0cf4022cb686f6ed7345cef4
Richard Dalton <richard@devjoy.com>
2025-12-10 00:46:11 +0000

    Added specific state transitions for handling operators

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,14 +3,26 @@ initial_state = {
     display_value: "0",
     operator: null,
 
-    handle_cancel: reset_state,
-    handle_negate: do_nothing,
-    handle_number: first_number,
-    handle_zero: do_nothing,
-    handle_operator: do_nothing,
-    handle_decimal: append_decimal,
-    handle_equal: apply_equal,
+    handle_cancel: reset_state,         // Always reset, this won't change
+    handle_equal: apply_equal,          // Always evaluate, this won't change
+    handle_number: first_number,        // First digit overwrites display
+    handle_decimal: append_decimal,     // Can immediately append decimal to initial 0
+    handle_zero: do_nothing,            // Zero does nothing, until a non-zero digit, or decimal is pressed
+    handle_negate: do_nothing,          // Can not negate 0
+    handle_operator: do_nothing,        // Nothing for operator to do until a number is entered
 };
+
+// Operator Selector, cycles through the sequence of operators
+function select_operator_handler() {
+    switch (state.handle_operator.name) {
+        case 'do_nothing':
+            return first_operator;
+        case 'first_operator':
+            return next_operator;
+        case 'next_operator':
+            return next_operator;
+    }
+}
 
 
 let state = Object.create(initial_state);
@@ -74,49 +86,55 @@ function reset_state() {
     refresh_display();
 }
 
+function apply_equal() {
+    expression = state.value.toString() + state.operator + state.display_value;
+    state.value = eval(expression);
+    state.display_value = state.value.toString();
+    state.handle_number = first_number;
+    state.handle_decimal = append_decimal;
+    return;
+}
+
 function negate_value() {
     state.display_value = state.display_value * -1;
-    console.log(state.value)
-    console.log(state.display_value)
     return;
 }
 
 function first_number(value) {
     state.display_value = value;
     state.handle_number = append_number;
-    state.handle_operator = apply_operator;
+    state.handle_operator = select_operator_handler();
     state.handle_decimal = append_decimal;
     state.handle_negate = negate_value;
     state.handle_zero = append_number;
-}
-
-function apply_operator(val) {
-    if (state.operator) {
-        expression = state.value.toString() + state.operator + state.display_value;
-        state.value = eval(expression);
-        state.display_value = state.value.toString();
-    }
-
-    state.operator = val;
-    state.value = Number(state.display_value);
-    state.handle_number = first_number;
-    state.handle_decimal = zero_decimal;
-    return;
-}
-
-function apply_equal() {
-    expression = state.value.toString() + state.operator + state.display_value;
-    console.log(expression);
-
-    state.value = eval(expression);
-    state.display_value = state.value.toString();
-    state.handle_number = first_number;
-    state.handle_decimal = do_nothing;
     return;
 }
 
 function append_number(value) {
     state.display_value += value;
+}
+
+
+function first_operator(val) {
+    state.operator = val;
+    state.value = Number(state.display_value);
+    common_operator_behaviour()
+    return;
+}
+
+function next_operator(val) {
+    expression = state.value.toString() + state.operator + state.display_value;
+    state.value = eval(expression);
+    common_operator_behaviour();
+    return
+}
+
+function common_operator_behaviour() {
+    state.display_value = state.value.toString();
+    state.handle_number = first_number;
+    state.handle_decimal = zero_decimal;
+    state.handle_operator = select_operator_handler();
+    return
 }
 
 function zero_decimal() {
